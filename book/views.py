f2d6b93f69a26cf745b53e2223c113fe5594221c
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-02-03 19:16:12 +0600

    Updated for filtering books by user name

--- book/views.py
+++ book/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from rest_framework.response import Response
 from rest_framework import status
 from . import models
 from . import serializers
 # Create your views here.
+class BooksWithSpecificUser(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        user = request.query_params.get("user")
+        if user:
+            return queryset.filter(user=user)
+        return queryset
+    
 class BookViewset(viewsets.ModelViewSet):
     queryset = models.Book.objects.all()
     serializer_class = serializers.BookSerializer
+    filter_backends = [BooksWithSpecificUser]
 
     def perform_create(self, serializer):
         
@@ -17,3 +25,4 @@ class BookViewset(viewsets.ModelViewSet):
         
         response = super().create(request, *args, **kwargs)
         return Response({'message': 'Book listed for donation successfully.'}, status=status.HTTP_201_CREATED)
+    
