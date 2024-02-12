c6539c1e598defc8b4e28e5ae10d926ef6058796
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-02-13 02:16:09 +0600

    Garbedge comment deleted

--- book/serializers.py
+++ book/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from . import models
 from django.contrib.auth.models import User
 class BookSerializer(serializers.ModelSerializer):
-    # user = serializers.StringRelatedField(many=False, required=False)
     class Meta:
         model = models.Book
         fields = '__all__'
