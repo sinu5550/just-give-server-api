6f31b96bba1eb02694c0bda8423cc48f2a85b1b6
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-02-13 02:43:21 +0600

    image media problem osled

--- gift/models.py
+++ gift/models.py
@@ -6,7 +6,7 @@ class Gift(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     coin_price = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='book/images/', default='book/images/gift-box.png')
+    image = models.ImageField(upload_to='gift/media/images/', default='gift/media/images/gift-box.png')
 
     def __str__(self):
         return self.name
