1105e6fc674391719521e198e975c0d566faed27
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-07-02 23:04:28 +0600

    [Updated] SMTP email pass refreshed

--- Just_Give_Server_site/settings.py
+++ Just_Give_Server_site/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://justgive-api-sinu5550.onrender.com','https://*.127.0.0.1']
+
 CORS_ALLOWED_ORIGINS = [
     "https://justgive-sinu5550.onrender.com",
     "http://127.0.0.1:5500",
